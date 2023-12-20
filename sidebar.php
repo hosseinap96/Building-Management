e09d1c022a031d193b2f5a21c2118f962b01251e
unknown <hosseinap96@gmail.com>
2023-12-20 17:57:51 +0330

    add sidebar to project

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<div class="col-md-2 h-100 d-flex flex-column sidebar">
+    <div>
+        <i class="fa-solid fa-home p-1"></i>
+        <a href="#">خانه </a>
+    </div>
+    <div>
+        <i class="fa-solid fa-building p-1"></i>
+        <a href="#">ساختمان ها</a>
+    </div>
+    <div>
+        <i class="fa-solid fa-right-from-bracket p-1"></i>
+        <a href="#">خروج</a>
+    </div>
+
+
+</div>
