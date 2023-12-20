504d4b4f4f2c2e7fcdb2a4f8d3d1a2e5538a0224
unknown <hosseinap96@gmail.com>
2023-12-20 17:59:35 +0330

    add index and add bootstrap - nav - sidebar

---
+++ index.php
@@ -0,0 +1,27 @@
+<html dir="rtl">
+
+<head>
+  <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.rtl.css">
+  <link rel="stylesheet" href="assets/fontawesome/css/fontawesome.css">
+  <link rel="stylesheet" href="assets/fontawesome/css/solid.css">
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/fontawesome/js/fontawesome.js"></script>
+</head>
+
+<body>
+
+
+  <?php
+  require_once('sidebar.php');
+  ?>
+
+  <!-- Content -->
+
+
+
+  <!-- End Content -->
+</body>
+
+</html>
