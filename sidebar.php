d9bee7073cab6857ebae30a8b4dab21d14ac2817
unknown <hosseinap96@gmail.com>
2023-12-22 22:25:42 +0330

    edit link color

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,15 @@
 <div class="col-md-2 h-100 d-flex flex-column sidebar">
     <div class="active">
         <i class="fa-solid fa-home p-1"></i>
-        <a href="#">خانه </a>
+        <a class="menu-link" href="#">خانه </a>
     </div>
     <div>
         <i class="fa-solid fa-building p-1"></i>
-        <a href="#">ساختمان ها</a>
+        <a class="menu-link" href="#">ساختمان ها</a>
     </div>
     <div>
         <i class="fa-solid fa-right-from-bracket p-1"></i>
-        <a href="#">خروج</a>
+        <a class="menu-link" href="#">خروج</a>
     </div>
 </div>
 
